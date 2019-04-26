6a0e584306784743ba9bd0a02d4a6a92f26bf812
Zoltan Koleszar <koleszr.zoltan@gmail.com>
2019-04-26 20:05:53 +0200

    add REST API endpoint for searching cheapest flights between dates
    Signed-off-by: Zoltan Koleszar <koleszr.zoltan@gmail.com>

--- travel/flights.py
+++ travel/flights.py
@@ -7,33 +7,71 @@ from psycopg2.extras import RealDictCursor
 
 bp = Blueprint('flights', __name__, url_prefix='/flights')
 
+DAILY_CHEAPEST_AT_QUERY = f'''
+SELECT *
+FROM flight_info
+WHERE
+departure_dt::date = %s AND
+price = (SELECT MIN(price) FROM flight_info WHERE departure_dt::date = %s AND departure_station = %s AND arrival_station = %s) AND
+departure_station = %s AND
+arrival_station = %s;
+'''
+
+DAILY_CHEAPEST_BETWEEN_QUERY = f'''
+SELECT flight_info.company, flight_info.departure_dt, flight_info.arrival_dt, flight_info.price FROM flight_info
+INNER JOIN (
+SELECT departure_dt::date as ddt, min(price) as mp
+FROM flight_info
+WHERE departure_station = %s AND arrival_station = %s AND departure_dt::date BETWEEN %s AND %s
+GROUP BY departure_dt::date
+) cheapest
+ON flight_info.departure_dt::date = cheapest.ddt AND flight_info.price = cheapest.mp
+WHERE flight_info.departure_station = %s AND flight_info.arrival_station = %s AND flight_info.departure_dt::date BETWEEN %s AND %s
+GROUP BY flight_info.price, flight_info.departure_dt, flight_info.arrival_dt, flight_info.company
+ORDER BY flight_info.departure_dt;
+'''
+
+FLIGHTS_QUERY = f'''
+SELECT * FROM flight_info
+WHERE departure_station = %s AND arrival_station = %s AND departure_dt::date = %s;
+'''
+
 def converter(o):
     if isinstance(o, datetime.datetime):
         return o.__str__()
     return o.__dict__
 
-@bp.route('/cheapest/<departure_station>/<arrival_station>/<date>')
-def cheapest(departure_station, arrival_station, date):
-    print('Getting cheapest flights...')
+@bp.route('/cheapest/<departure_station>/<arrival_station>/<from_date>')
+@bp.route('/cheapest/<departure_station>/<arrival_station>/<from_date>/<to_date>')
+def cheapest(departure_station, arrival_station, from_date, to_date=None):
+    if to_date is None:
+        return daily_cheapest_at(departure_station, arrival_station, from_date)
+    else:
+        return daily_cheapest_between(departure_station, arrival_station, from_date, to_date)
 
-    from_dt = f'{date} 00:00:00'
-    to_dt = f'{date} 23:59:59'
+def daily_cheapest_at(departure_station, arrival_station, date):
+    print(f'Getting cheapest flight on {date}...')
+
+    params = (date, date, departure_station, arrival_station, departure_station, arrival_station)
 
     db = get_db()
-    cursor = db.cursor(cursor_factory=RealDictCursor)
-    cursor.execute(
-        f'''
-        SELECT *
-        FROM flight_info
-        WHERE 
-        departure_dt BETWEEN %s AND %s AND
-        price = (SELECT MIN(price) FROM flight_info WHERE departure_dt BETWEEN %s AND %s AND departure_station = %s AND arrival_station = %s) AND
-        departure_station = %s AND
-        arrival_station = %s;
-        ''',
-        (from_dt, to_dt, from_dt, to_dt, departure_station, arrival_station, departure_station, arrival_station)
-    )
 
+    cursor = db.cursor(cursor_factory=RealDictCursor)
+    cursor.execute(DAILY_CHEAPEST_AT_QUERY, params)
+    flights = cursor.fetchall()
+    cursor.close()
+
+    return jsonify(flights)
+
+def daily_cheapest_between(departure_station, arrival_station, from_date, to_date):
+    print(f'Getting cheapest flights between {from_date} and {to_date}...')
+
+    params = (departure_station, arrival_station, from_date, to_date, departure_station, arrival_station, from_date, to_date)
+
+    db = get_db()
+
+    cursor = db.cursor(cursor_factory=RealDictCursor)
+    cursor.execute(DAILY_CHEAPEST_BETWEEN_QUERY, params)
     flights = cursor.fetchall()
     cursor.close()
 
@@ -43,21 +81,11 @@ def cheapest(departure_station, arrival_station, date):
 def flights(departure_station, arrival_station, date):
     print(f'Getting flights @ {date}...')
 
-    from_dt = f'{date} 00:00:00'
-    to_dt = f'{date} 23:59:59'
+    params = (departure_station, arrival_station, date)
 
     db = get_db()
     cursor = db.cursor(cursor_factory=RealDictCursor)
-    cursor.execute(
-        f'''
-        SELECT * FROM flight_info 
-        WHERE
-        departure_station = %s AND
-        arrival_station = %s AND
-        departure_dt BETWEEN %s AND %s;
-        ''',
-        (departure_station, arrival_station, from_dt, to_dt)
-    )
+    cursor.execute(FLIGHTS_QUERY, params)
 
     flights = cursor.fetchall()
     cursor.close()
